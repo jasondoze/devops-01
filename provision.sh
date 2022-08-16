f8a50bd3c93f99e76d2ba05886f6b52bde17c4bd
Jason Doze <jdoze@protonmail.com>
2022-08-16 16:20:20 -0500

    Add viking to devOps1.
    Signed-off-by: Jason Doze <jdoze@protonmail.com>

--- provision.sh
+++ provision.sh
@@ -12,6 +12,12 @@ sudo cp -r /vagrant/website /var/www/website
 echo "copy website.config file to sites-enabled nginx"
 sudo cp /vagrant/website.config /etc/nginx/sites-enabled/website
 
+echo 'copy viking to server'
+sudo cp -r /vagrant/viking /var/www/viking
+
+echo "copy viking.config file to sites-enabled nginx"
+sudo cp /vagrant/website.config /etc/nginx/sites-enabled/viking
+
 echo "restart the nginx service"
 sudo systemctl reload nginx
 
@@ -23,3 +29,6 @@ sudo df -h
 
 echo "display website html"
 curl 192.168.56.81:81
+
+echo "display viking html"
+curl 192.168.56.81:86
